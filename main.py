8a1f6abb220ed16615c1ca4959ab1777beb6f914
henntaidesu <58099835+henntaidesu@users.noreply.github.com>
2024-07-11 23:40:46 +0800

    commit

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import logging
 import requests
 import feedparser
 import sqlite3
-from sqlite3 import IntegrityError, OperationalError
 import opencc
-from datetime import datetime
 import time
 import os
+import re
+import threading
+from sqlite3 import IntegrityError, OperationalError
+from datetime import datetime
+
 
 converter = opencc.OpenCC('t2s')  # 设置繁简转化
 
@@ -19,7 +22,7 @@ proxies = {
 }
 down_path = "/qbittorrent/anime/data/Anime"
 log_path = "/qbittorrent/anime/autoRss/log"
-stop_time = 600  # 每次获取RSS间隔
+stop_time = 1200  # 每次获取RSS间隔 单位秒
 
 
 def now_time():
@@ -133,40 +136,62 @@ class DB:
         self.conn.close()
 
 
-def qbittorrent(save_path, torrent_file, quarter):
-    # 登录qBittorrent Web UI
-    login_url = f'{qb_url}/api/v2/auth/login'
-    login_data = {'username': username, 'password': password}
-    session = requests.Session()
-    response = session.post(login_url, data=login_data)
+class qbittorrent:
+    @staticmethod
+    def login():
+        login_url = f'{qb_url}/api/v2/auth/login'
+        login_data = {'username': username, 'password': password}
+        session = requests.Session()
+        response = session.post(login_url, data=login_data)
+        return response, session
 
-    if not os.path.exists(save_path):
-        os.makedirs(save_path)
-        time.sleep(1)
-        os.chmod(save_path, 0o777)
+    @staticmethod
+    def add_torrent(save_path, torrent_file, quarter):
+        # 登录qBittorrent Web UI
 
-    if response.status_code == 200:
-        # 添加下载任务
-        add_url = f'{qb_url}/api/v2/torrents/add'
-        with open(torrent_file, 'rb') as f:
-            files = {'torrents': f}
-            data = {
-                'savepath': save_path,
-                'autoTMM': 'false',  # 禁用自动分类
-                'paused': 'false',  # 立即开始下载
-                # 'skip_checking': 'true',  # 跳过文件校验
-                'category': 'autoRSS',
-                'tags': quarter
-            }
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+            time.sleep(1)
+            os.chmod(save_path, 0o777)
 
-            response = session.post(add_url, data=data, files=files)
+        response, session = qbittorrent.login()
         if response.status_code == 200:
-            return True
+            # 添加下载任务
+            add_url = f'{qb_url}/api/v2/torrents/add'
+            with open(torrent_file, 'rb') as f:
+                files = {'torrents': f}
+                data = {
+                    'savepath': save_path,
+                    'autoTMM': 'false',  # 禁用自动分类
+                    'paused': 'false',  # 立即开始下载
+                    # 'skip_checking': 'true',  # 跳过文件校验
+                    'category': f'autoRSS {quarter}',
+                    'tags': quarter
+                }
+
+                response = session.post(add_url, data=data, files=files)
+            if response.status_code == 200:
+                return True
+            else:
+                Log().write_log(f"添加下载任务失败: {response.text}", 'error')
         else:
-            print(f"添加下载任务失败: {response.text}")
-    else:
-        print(f"登录失败: {response.text}")
-        print(response.status_code)
+            Log().write_log(f"登录失败: {response.text}", 'error')
+            Log().write_log(response.status_code, 'error')
+
+    @staticmethod
+    def get_torrent_list():
+        torrent_list = []
+        response, session = qbittorrent.login()
+        if response.status_code == 200:
+            get_list_url = f'{qb_url}/api/v2/torrents/info'
+            torrent_web_list = session.get(get_list_url).json()
+            for i in torrent_web_list:
+                torrent_list.append(i['name'])
+
+            return torrent_list
+
+        else:
+            print('err')
 
 
 def nyaa_ANI_torrent():
@@ -269,49 +294,88 @@ def nyaa_ANI_torrent():
          VALUES ('ANI', '{torrent_url}', '{anime_title}', '{number_of_words}', 0, {season}, '{release_time}', 'nyaa');'''
         if DB().insert(sql):
             Log().write_log(f'已更新订阅 - ANI - {anime_title} - 第{season}季 - 第{number_of_words}集', 'info')
+            auto_download(torrent_url, 'nyaa', anime_title, number_of_words, season)
         else:
-            Log().write_log('订阅无新内容', 'info')
+            # Log().write_log(f'订阅无新内容,休眠{stop_time}秒', 'info')
             return False
-    return True
 
 
-def auto_download():
-    if nyaa_ANI_torrent():
-        sql = f"SELECT * FROM rss_torrent WHERE status = '0'"
-        data = DB().select(sql)
-        for i in data:
-            print(i)
-            torrent_id = i[1]
-            torrent_from = i[2]
-            anime_title = i[3]
-            number_of_words = i[4]
-            season = i[6]
-            sql = f'''SELECT quarter FROM "anime_quarter" WHERE anime_name = "{anime_title}"'''
-            quarter = DB().select(sql)
-            if quarter:
-                quarter = quarter[0][0]
-                if torrent_from == 'nyaa':
-                    torrent_url = f'''https://nyaa.si/download/{torrent_id}.torrent'''
-                    save_path = f"{down_path}/{quarter}/{anime_title}/Season {season}"
+def auto_download(torrent_id, torrent_from, anime_title, number_of_words, season):
+    # sql = f"SELECT * FROM rss_torrent WHERE status = '0'"
+    # data = DB().select(sql)
+    # for i in data:
+    #     print(i)
+    #     torrent_id = i[1]
+    #     torrent_from = i[2]
+    #     anime_title = i[3]
+    #     number_of_words = i[4]
+    #     season = i[6]
 
-                    response = requests.get(torrent_url, proxies=proxies)
-                    torrent_file = f'torrent/{torrent_id}.torrent'
-                    with open(torrent_file, 'wb') as f:
-                        f.write(response.content)
+    sql = f'''SELECT quarter FROM "anime_quarter" WHERE anime_name = "{anime_title}"'''
+    quarter = DB().select(sql)
+    if quarter:
+        quarter = quarter[0][0]
+        if torrent_from == 'nyaa':
+            torrent_url = f'''https://nyaa.si/download/{torrent_id}.torrent'''
+        save_path = f"{down_path}/{quarter}/{anime_title}/Season {season}"
 
-                    if qbittorrent(save_path, torrent_file, quarter):
-                        Log().write_log(f'添加QB成功 - {anime_title} - 第{season}季 - 第{number_of_words}集', 'info')
-                        sql = f'''UPDATE "main"."rss_torrent" SET  "status" = '1' WHERE "torrent_url" = '{torrent_id}';'''
-                        DB().updata(sql)
+        response = requests.get(torrent_url, proxies=proxies)
+        torrent_file = f'torrent/{torrent_id}.torrent'
+        with open(torrent_file, 'wb') as f:
+            f.write(response.content)
 
-                    os.remove(torrent_file)
-
-
-def main():
-    while True:
-        auto_download()
-        Log().write_log(f'休眠{stop_time}秒', 'info')
-        time.sleep(stop_time)
+        if qbittorrent().add_torrent(save_path, torrent_file, quarter):
+            # Log().write_log(f'添加QB成功 - {anime_title} - 第{season}季 - 第{number_of_words}集', 'info')
+            sql = f'''UPDATE "main"."rss_torrent" SET  "status" = '1' WHERE "torrent_url" = '{torrent_id}';'''
+            DB().updata(sql)
+        os.remove(torrent_file)
+    else:
+        Log().write_log(f'无匹配 - {anime_title} - 第{season}季 - 第{number_of_words}集', 'warning')
 
 
-main()
+def robot(message):
+    try:
+        requests.get(f"http://172.16.1.1:3000/send_group_msg?group_id=&message={message}")
+        time.sleep(1)
+        requests.get(f"http://172.16.1.1:3000/send_private_msg?user_id=&message={message}")
+    except Exception as e:
+        Log().write_log(e, 'error')
+
+
+class main:
+    @staticmethod
+    def new_torrent():
+        torrent_old_list = qbittorrent().get_torrent_list()
+        while True:
+            torrent_new_list = qbittorrent().get_torrent_list()
+            new_torrent = [item for item in torrent_new_list if item not in torrent_old_list]
+
+            if new_torrent:
+                for i in new_torrent:
+                    if 'ANi' in i:
+                        title = re.sub(r'\[.*?\]', '', i).replace(' ', '')
+                    else:
+                        anime_title = i
+
+                    anime_title = converter.convert(title)
+                    robot(f'已获取 - {anime_title}')
+                    Log().write_log(f'已获取新番剧 - {anime_title}', 'info')
+
+                torrent_old_list = torrent_new_list
+            else:
+                if len(torrent_new_list) < len(torrent_old_list):
+                    torrent_old_list = torrent_new_list
+                time.sleep(10)
+
+    @staticmethod
+    def get_Rss():
+        Log().write_log('程序启动', 'info')
+        while True:
+            nyaa_ANI_torrent()
+            time.sleep(stop_time)
+
+
+thread1 = threading.Thread(target=main().new_torrent)
+thread2 = threading.Thread(target=main().get_Rss)
+thread1.start()
+thread2.start()
