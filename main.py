49f6fce732e33bf4de158ecc0abcb025fd0fc530
makuro shiina <58099835+henntaidesu@users.noreply.github.com>
2025-07-01 19:05:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 import requests
 import feedparser
 import sqlite3
@@ -11,17 +12,24 @@ from sqlite3 import IntegrityError, OperationalError
 from datetime import datetime
 import pymysql
 
+
 converter = opencc.OpenCC('t2s')  # 设置繁简转化
 
 qb_url = 'http://qbittorrent.home.makuro.cn:8082'  # qBittorrent Web UI的URL
-username = 'username'  # qBittorrent Web UI的用户名
-password = 'password'  # qBittorrent Web UI的密码
+username = 'makuro'  # qBittorrent Web UI的用户名
+password = 'SRCak2244@'  # qBittorrent Web UI的密码
 proxies = {
     'http': 'http://172.16.1.10:10811',
     'https': 'http://172.16.1.10:10811'
 }
-down_path = "/qbittorrent/anime/data/Anime"
-log_path = "/qbittorrent/anime/autoRss/log"
+down_path = "/qbittorrent/vcbs/nvme_data/Anime"
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+log_path = f"{current_dir}/log"
+torrent_path = f"{current_dir}/torrent"
+os.makedirs(log_path, exist_ok=True) 
+os.makedirs(torrent_path, exist_ok=True) 
+
 stop_time = 1200  # 每次获取RSS间隔 单位秒
 
 mysql_host = '172.16.1.22'
@@ -49,9 +57,10 @@ class Log:
         self.day = today()
         self.logger = self.setup_logger()
         self.log_level = 'debug'
+        
 
     def setup_logger(self):
-        log_file = f"log/{self.day}.log"
+        log_file = f"{log_path}/{self.day}.log"
         # 创建一个logger对象
         logger = logging.getLogger("my_logger")
         logger.setLevel(logging.DEBUG)
@@ -218,7 +227,7 @@ class qbittorrent:
             Log().write_log(response.status_code, 'error')
 
     @staticmethod
-    def get_torrent_list():
+    def get_qbittorrent_torrent_list():
         torrent_list = []
         response, session = qbittorrent.login()
         if response.status_code == 200:
@@ -234,14 +243,16 @@ class qbittorrent:
 
 
 def nyaa_ANI_torrent():
+    # Log().write_log(f"启动", 'warning')
     res_url = 'https://nyaa.si/?page=rss&u=ANiTorrent'
-    response = requests.get(res_url, proxies=proxies)
+    response = requests.get(res_url, proxies=proxies, timeout=10)
     feed_content = response.content
 
     # 解析 RSS feed
     feed = feedparser.parse(feed_content)
-
+    limit = 1
     for entry in feed.entries:
+        limit += 1
         number_of_words = None
         torrent_url = entry.link
         torrent_url = torrent_url[torrent_url.rfind('/') + 1:]
@@ -257,91 +268,98 @@ def nyaa_ANI_torrent():
             title = title[title.find(']') + 1:].replace(' ', '')
 
         number_of_words = re.sub(r'\[[^\]]*\]', '', torrent_title)
-        number_of_words = int(number_of_words[number_of_words.rfind('-') + 1:].lstrip().rstrip())
+        try:
+            number_of_words = int(number_of_words[number_of_words.rfind('-') + 1:].lstrip().rstrip())
+        except ValueError:
+            Log().write_log(f'集数转化错误', 'error')
+            if number_of_words == '特别篇':
+                number_of_words = -1
+            else:
+                Log().write_log(f'集数转化错误 ---- {number_of_words}', 'error') 
+                number_of_words = -2
 
         anime_title = converter.convert(title)
 
         release_time = datetime.strptime(entry.get('published', 'No published time'),
                                          '%a, %d %b %Y %H:%M:%S %z').replace(tzinfo=None)
 
-        if '第十季' in torrent_title:
-            season = 10
-        elif '第九季' in torrent_title:
-            season = 9
-        elif '第八季' in torrent_title:
-            season = 8
-        elif '第七季' in torrent_title:
-            season = 7
-        elif '第六季' in torrent_title:
-            season = 6
-        elif '第五季' in torrent_title:
-            season = 5
-        elif '第四季' in torrent_title:
-            season = 4
-        elif '第三季' in torrent_title:
-            season = 3
-        elif '第二季' in torrent_title:
-            season = 2
+        # 默认 season 值
+        season = 1
+
+        # 中文季数标签（按优先顺序）
+        cn_seasons = ['第十季', '第九季', '第八季', '第七季', '第六季',
+                    '第五季', '第四季', '第三季', '第二季', '第一季']
+
+        # 英文季数标签（按优先顺序）
+        en_seasons = ['Season10', 'Season9', 'Season8', 'Season7', 'Season6',
+                    'Season5', 'Season4', 'Season3', 'Season2', 'Season1']
+
+        # 提取 season
+        for i, tag in enumerate(cn_seasons):
+            if tag in torrent_title:
+                season = 10 - i
+                break
         else:
-            season = 1
+            for i, tag in enumerate(en_seasons):
+                if tag in torrent_title:
+                    season = 10 - i
+                    break
 
-        if '第十季' in torrent_title:
-            anime_title = anime_title.replace('第十季', '')
-        elif '第九季' in torrent_title:
-            anime_title = anime_title.replace('第九季', '')
-        elif '第八季' in torrent_title:
-            anime_title = anime_title.replace('第八季', '')
-        elif '第七季' in anime_title:
-            anime_title = anime_title.replace('第七季', '')
-        elif '第六季' in anime_title:
-            anime_title = anime_title.replace('第六季', '')
-        elif '第五季' in anime_title:
-            anime_title = anime_title.replace('第五季', '')
-        elif '第四季' in anime_title:
-            anime_title = anime_title.replace('第四季', '')
-        elif '第三季' in anime_title:
-            anime_title = anime_title.replace('第三季', '')
-        elif '第二季' in anime_title:
-            anime_title = anime_title.replace('第二季', '')
-        elif '第一季' in anime_title:
-            anime_title = anime_title.replace('第一季', '')
+        # 清理 anime_title 中的中文标签
+        for tag in cn_seasons:
+            if tag in anime_title:
+                anime_title = anime_title.replace(tag, '')
 
-        if number_of_words in ('01', '特别篇'):
-            year = str(release_time.year)
-            month = str(release_time.month)
+        # 清理 anime_title 中的英文标签
+        for tag in en_seasons:
+            if tag in anime_title:
+                anime_title = anime_title.replace(tag, '')
+        
+        # Log().write_log(f"{anime_title} ---  {number_of_words}", 'warning')
 
-            if month in ('12', '1', '2'):
-                month = 'A'
-            elif month in ('3', '4', '5'):
-                month = 'B'
-            elif month in ('6', '7', '8'):
-                month = 'C'
-            elif month in ('9', '10', '1'):
-                month = 'D'
+        try:
+            if number_of_words in (1, -1):
+                year = release_time.year
+                month = release_time.month
 
-            if month == '12':
-                year = str(int(year) + 1)
-            year = year[2:]
+                if month == 12:
+                    year += 1
+                    season_code = 'A'
+                elif month in (1, 2):
+                    season_code = 'A'
+                elif month in (3, 4, 5):
+                    season_code = 'B'
+                elif month in (6, 7, 8):
+                    season_code = 'C'
+                else:
+                    season_code = 'D'
 
-            quarter = f"{year}{month}"
+                quarter = f"{str(year)[2:]}{season_code}"
 
-            sql = f'''INSERT INTO `anime_quarter` (`anime_name`, `quarter`) VALUES ('{anime_title}', '{quarter}');'''
-            DB().insert(sql)
+                sql = f'''INSERT INTO `anime_quarter` (`anime_name`, `quarter`, `season`) VALUES ('{anime_title}', '{quarter}', '{season}')'''
+                DB().insert(sql)
+        except Exception as e:
+            Log().write_log(f'季度计算错误 - {anime_title} - {number_of_words} - {release_time}', 'error')
 
         sql = f'''INSERT INTO `rss_torrent`
         (`rss_group`, `torrent_url`, `anime_title`, `number_of_words`, `status`, `season`, `release_time`, `torrent_from`)
          VALUES ('ANI', '{torrent_url}', '{anime_title}', {number_of_words}, 0, {season}, '{release_time}', 'nyaa');'''
         if DB().insert(sql):
             Log().write_log(f'已更新订阅 - ANI - {anime_title} - 第{season}季 - 第{number_of_words}集', 'info')
-            auto_download(torrent_url, 'nyaa', anime_title, number_of_words, season)
+            # auto_download(torrent_url, 'nyaa', anime_title, number_of_words, season)
         else:
+            auto_download(limit)
             # Log().write_log(f'订阅无新内容,休眠{stop_time}秒', 'info')
             return False
+    
+    
 
 
 # def auto_download(torrent_id, torrent_from, anime_title, number_of_words, season):
-def auto_download():
-    sql = f"SELECT * FROM rss_torrent WHERE status = '0'"
+    #     pass
+
+def auto_download(limit):
+    sql = f"SELECT * FROM rss_torrent WHERE status = '0' order by release_time desc limit {limit + 1}"
     flag, data = DB().select(sql)
     for i in data:
         print(i)
@@ -350,7 +368,6 @@ def auto_download():
         anime_title = i[3]
         number_of_words = i[4]
         season = i[6]
-
         sql = f'''SELECT quarter FROM `anime_quarter` WHERE anime_name = "{anime_title}"'''
         flag, quarter = DB().select(sql)
         if quarter:
@@ -360,58 +377,64 @@ def auto_download():
             save_path = f"{down_path}/{quarter}/{anime_title}/Season {season}"
 
             response = requests.get(torrent_url, proxies=proxies)
-            torrent_file = f'torrent/{torrent_id}.torrent'
-            with open(torrent_file, 'wb') as f:
-                f.write(response.content)
+            torrent_file = f'{torrent_path}/{torrent_id}.torrent'
+            try:
+                with open(torrent_file, 'wb') as f:
+                    f.write(response.content)
+            except FileNotFoundError:
+                Log().write_log(f'下载种子错误 url - {torrent_url}', 'error')
 
             if qbittorrent().add_torrent(save_path, torrent_file, quarter):
                 Log().write_log(f'添加QB成功 - {anime_title} - 第{season}季 - 第{number_of_words}集', 'info')
                 sql = f'''UPDATE `rss_torrent` SET  `status` = '1' WHERE `torrent_url` = '{torrent_id}';'''
                 DB().update(sql)
-            os.remove(torrent_file)
+            # os.remove(torrent_file)
         else:
             Log().write_log(f'无匹配 - {anime_title} - 第{season}季 - 第{number_of_words}集', 'warning')
 
 
 def robot(message):
-    pass
-    # try:
-    #     req = requests.get(f"http://172.16.1.19:3000/send_group_msg?group_id=651936926&message={message}")
-    #     if req.status_code == 200:
-    #         if req.json()['status'] == 'ok':
-    #             Log().write_log(f"OK - {message}", 'error')
-    #     time.sleep(1)
-    #
-    #     for _ in range(3):
-    #         req = requests.get(f"http://172.16.1.19:3000/send_private_msg?user_id=498791444&message={message}")
-    #         if req.status_code == 200:
-    #             if req.json()['status'] == 'ok':
-    #                 Log().write_log(f"OK - {message}", 'error')
-    #                 break
-    #         else:
-    #             Log().write_log(f"{req.status_code} - {message}", 'error')
-    # except Exception as e:
-    #     Log().write_log(e, 'error')
+    try:
+        requests.get(f"http://172.16.1.19:3000/send_group_msg?group_id=971990897&message={message}")
+        # if req.status_code == 200:
+        #     if req.json()['status'] == 'ok':
+        #         Log().write_log(f"OK - {message}", 'error')
+        # time.sleep(1)
+
+        # for _ in range(3):
+        #     req = requests.get(f"http://172.16.1.19:3000/send_private_msg?user_id=498791444&message={message}")
+        #     if req.status_code == 200:
+        #         if req.json()['status'] == 'ok':
+        #             Log().write_log(f"OK - {message}", 'info')
+        #             break
+        #     else:
+        #         Log().write_log(f"{req.status_code} - {message}", 'error')
+    except Exception as e:
+        Log().write_log(e, 'error')
+
+
+# nyaa_ANI_torrent()
 
 
 class main:
     @staticmethod
     def new_torrent():
-        torrent_old_list = qbittorrent().get_torrent_list()
+        torrent_old_list = qbittorrent().get_qbittorrent_torrent_list()
         while True:
-            torrent_new_list = qbittorrent().get_torrent_list()
+            torrent_new_list = qbittorrent().get_qbittorrent_torrent_list()
             new_torrent = [item for item in torrent_new_list if item not in torrent_old_list]
 
             if new_torrent:
                 for i in new_torrent:
                     anime_title = i
-                    anime_title = (str(anime_title).replace('[ANi] ', '').replace('[1080P]', '').
-                                   replace('[Baha]', '').replace('[Nekomoe kissaten]', '').
-                                   replace('[BDRip]', '').replace('[JPSC]', '').
-                                   replace('.mp4', '').replace('[WEB-DL]', '').
-                                   replace('[AAC AVC]', '').replace('[CHT]', '').
-                                   replace('[Bilibili]', '').replace('[CHT CHS]', '').
-                                   replace('（仅限港澳台）', ''))
+
+                    replacements = ['[ANi] ', '[1080P]', '[Baha]', '[Nekomoe kissaten]', '[BDRip]', '[JPSC]',
+                                    '.mp4', '[WEB-DL]', '[AAC AVC]', '[CHT]', '[Bilibili]', '[CHT CHS]', '（仅限港澳台）']
+                    anime_title = str(anime_title)
+                    for item in replacements:
+                        anime_title = anime_title.replace(item, '')
+
+                    
                     anime_title = converter.convert(anime_title)
                     robot(f'已获取 - {anime_title}')
                     Log().write_log(f'已获取新番剧 - {anime_title}', 'info')
@@ -430,9 +453,18 @@ class main:
             time.sleep(stop_time)
 
 
-# thread1 = threading.Thread(target=main().new_torrent)
-# thread2 = threading.Thread(target=main().get_Rss)
-# thread1.start()
-# thread2.start()
+def start():
+    try:
+        thread1 = threading.Thread(target=main().new_torrent)
+        thread2 = threading.Thread(target=main().get_Rss)
+        thread1.start()
+        time.sleep(3)
+        thread2.start()
+        # robot('autoRss 启动')
+    except Exception as e:
+        Log().write_log(e, 'error')
+        start()
 
-auto_download()
+start()
+
+
